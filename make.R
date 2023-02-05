347718b857f17fa19f7e85628efebc1ec44d5ca6
skefi <sonia.kefi@umontpellier.fr>
2023-02-05 13:38:41 +0100

    Init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' spatialews_biocom: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Sonia Kefi \email{sonia.kefi@umontpellier.fr}
+#' 
+#' @date 2023/02/05
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
